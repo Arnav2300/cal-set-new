73ebd144c4e8a3b5186e94881a65929247ad936e
Arnav Saraswat <arnav.sar@gmail.com>
2024-12-14 20:03:32 +0000

    login service completed

--- user-management-service/api/services/login.go
+++ user-management-service/api/services/login.go
@@ -1,8 +1,31 @@
 package services
 
-func LoginService() {
-	//get email and password
-	//check if email is present, if not fail
-	//check if password hashes match, if not fail
-	//sign token and send it back along with account details
+import (
+	"context"
+	"errors"
+	"fmt"
+	"user-management-service/api/dto"
+	"user-management-service/api/repository"
+	"user-management-service/api/utils"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+func LoginService(ctx context.Context, q *repository.Queries, credentials dto.LoginDTO) (string, error) {
+	user, err := q.GetUserByEmail(ctx, pgtype.Text{String: credentials.Email, Valid: true})
+	if err != nil {
+		fmt.Printf("Error fetching user by email: %v", err)
+		return "", errors.New("incorrect email or password")
+	}
+	err = utils.VerifyPassword(user.Password.String, credentials.Password)
+	if err != nil {
+		fmt.Printf("Password mismatch for user: %s", credentials.Email)
+		return "", errors.New("incorrect email or password")
+	}
+	signedToken, err := utils.SignJwtToken(user.Role, credentials.Email, user.Username)
+	if err != nil {
+		fmt.Printf("Error signing JWT token: %v", err)
+		return "", errors.New("we ran into a problem")
+	}
+	return signedToken, nil
 }
